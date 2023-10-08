07622cd9746bf9ebd26f1320489c45b057db2df2
finn4522 <147115573+finn4522@users.noreply.github.com>
2023-10-08 15:44:27 -0700

    Update pi_estimate.py

--- pi_estimate.py
+++ pi_estimate.py
@@ -7,7 +7,8 @@ import doctest
 import points_plot
 
 GOOD_PI = 3.141592653589793  # A very good estimate, from math.pi
-SAMPLES = 1_00   # More =>  more precise, but slower
+SAMPLES = 5_000  # More =>  more precise, but slower
+
 
 def in_unit_circle(x: float, y: float) -> bool:
     """
@@ -16,27 +17,29 @@ def in_unit_circle(x: float, y: float) -> bool:
 
     >>> in_unit_circle(0.0,0.0)
     True
-   
+
     >>> in_unit_circle(1.0,1.0)
     False
 
     >>> in_unit_circle(0.5,-0.5)
     True
-  
+
     >>> in_unit_circle(-0.9,-0.5)
     False
     """
     squared = x**2 + y**2
     if squared < 1.0:
-        print(True)
+        return(True)
     else:
-        print(False)
+        return(False)
 
-def  rand_point_unit_sq() -> tuple[float, float]:
+
+def rand_point_unit_sq() -> tuple[float, float]:
     """Returns random x,y both in range 0..1.0, 0..1.0."""
     x = random.random()
     y = random.random()
     return x, y
+
 
 def plot_random_points(n_points: int = 500):
     """Generate and plot n_points points
@@ -50,6 +53,7 @@ def plot_random_points(n_points: int = 500):
         points_plot.plot(x, y, color_rgb=(50, 50, 50))
     points_plot.wait_to_close()
 
+
 def relative_error(est: float, expected: float) -> float:
     """Relative error of estimate (est) as non-negative fraction of expected value.
     Note estimate and expected are NOT interchangeable (see test cases).
@@ -62,10 +66,11 @@ def relative_error(est: float, expected: float) -> float:
     >>> round(relative_error(5.0, 3.0), 2)
     0.67
     """
-    
+
     abs_error = est - expected
     rel_error = abs(abs_error / expected)
     return rel_error
+
 
 def pi_approx() -> float:
     """Return an estimate of pi by sampling random points.
@@ -76,17 +81,17 @@ def pi_approx() -> float:
     >>> relative_error(pi_approx(), GOOD_PI) <= 0.01  # Within 1%
     True
     """
-   
+
     total_try = 0
     total_in_circle = 0
     for i in range(SAMPLES):
-        (f, g) = rand_point_unit_sq()
+        (x, y) = rand_point_unit_sq()
         total_try = total_try + 1
-        if in_unit_circle(f, g) == True:
+        if in_unit_circle(x, y) == True:
             total_in_circle = total_in_circle + 1
-            points_plot.plot(f, g, color_rgb=(255, 10, 10)) ## Red
+            points_plot.plot(x, y, color_rgb=(255, 10, 10))  ## Red
         else:
-            points_plot.plot(f, g, color_rgb=(240, 240, 240)) ## Light grey
+            points_plot.plot(x, y, color_rgb=(240, 240, 240))  ## Light grey
     return (total_in_circle / total_try) * 4
 
 
@@ -98,6 +103,6 @@ def main():
     print(f"Pi is approximately {estimate}")
     points_plot.wait_to_close()
 
+
 if __name__ == "__main__":
     main()
-
